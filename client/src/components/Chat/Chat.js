6cba848bee68501c3a87a7cd6aaf8233101c4e3f
HarshKunte <harshkunte99@gmail.com>
2020-11-07 00:10:04 +0530

    init

---
+++ client/src/components/Chat/Chat.js
@@ -0,0 +1,68 @@
+
+import React,{useState,useEffect} from 'react'
+import queryString from 'query-string';
+import './Chat.css'
+import io from 'socket.io-client'
+
+import TextContainer from '../TextContainer/TextContainer';
+import Messages from '../Messages/Messages';
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
+
+let socket;
+const Chat= ({location})=> {
+    const [name,setName] = useState('');
+    const [room,setRoom] = useState('');
+    const [messages,setMessages] = useState([]);
+    const [message,setMessage] = useState('');
+    const [users, setUsers] = useState('');
+
+    const ENDPOINT ='https://chatting-reactapp.herokuapp.com/';
+    useEffect(()=>{
+        const {name,room} = queryString.parse(location.search)
+
+        socket = io(ENDPOINT);
+
+        setName(name)
+        setRoom(room)
+
+        socket.emit('join',{name,room},()=>{
+        });
+
+        return ()=>{
+            socket.emit('disconnect')
+            socket.off();
+        }
+
+    },[ENDPOINT,location.search])
+
+    useEffect(()=>{
+        socket.on('message',(message)=>{
+            setMessages([...messages,message])
+        })
+        socket.on("roomData", ({ users }) => {
+            setUsers(users);
+          });
+    },[messages]);
+
+    const sendMessage = (event) => {
+        event.preventDefault();
+    
+        if(message) {
+          socket.emit('sendMessage', message, () => setMessage(''));
+        }
+      }
+    
+      return (
+        <div className="outerContainer">
+          <div className="container">
+              <InfoBar room={room} />
+              <Messages messages={messages} name={name} />
+              <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+          </div>
+          <TextContainer users={users}/>
+        </div>
+      );
+}
+
+export default Chat;
